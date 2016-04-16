ada23054817268d67db65aeeb25c4209df56726e
snigdhagoel <snigdhag@usc.edu>
2016-04-16 02:40:47 -0700

    Added EU MEU
    Added EU MEU

--- Assignment_3/hw3cs561s16.py
+++ Assignment_3/hw3cs561s16.py
@@ -3,7 +3,9 @@ from math import pow
 from copy import deepcopy
 
 positionMapping = {"+":0, "-":1, "++":0, "+-":1, "-+":2, "--":3, "+++":0, "++-":1, "+-+":2, "+--":3, "-++":4, "-+-":5, "--+":6, "---":7}
-
+output = open("output.txt", "w")
+utilityNode = []
+decisionNodes = []
 
 class Query:
     
@@ -28,6 +30,7 @@ class Node:
         self.parents = []
         self.children = []
         self.CPT = []
+        self.decision = False
         
     def addchild(self, child):
         if not child in self.children:
@@ -44,9 +47,16 @@ class Node:
             size = int(pow(2, len(self.parents)))
             self.CPT = [None] * size
             for idx in range(size):
-                pRow = inputProbs[idx].strip(' \t\n\r').split(' ')
-                tableIdx = positionMapping[''.join(pRow[1:])]
-                self.CPT[tableIdx] = float(pRow[0])
+                if not inputProbs[idx].strip(' \t\n\r') == "":
+                    pRow = inputProbs[idx].strip(' \t\n\r').split(' ')
+                    tableIdx = positionMapping[''.join(pRow[1:])]
+                    self.CPT[tableIdx] = float(pRow[0])
+    
+    def setDecisionTrue(self):
+        self.decision = True
+                
+    def isDecision(self):
+        return self.decision
         
     def printNode(self):
         print self.name
@@ -76,9 +86,15 @@ class BayesNet:
                 pNode = self.nodeMap[parent.strip(' \t\n\r')]
                 newNode.addParent(pNode)
                 pNode.addchild(newNode)
-        newNode.addTable(inputs[1:])
+        if inputs[1].strip(' \t\r\n') == "decision":
+            newNode.setDecisionTrue()
+            decisionNodes.append(newNode)
+        else:
+            newNode.addTable(inputs[1:])
         self.nodeMap[name] = newNode
         self.nodeList.append(newNode)
+        if newNode.name == "utility":
+            utilityNode.append(newNode)
 
     def parseInputGraph(self, data):
         
@@ -89,10 +105,12 @@ class BayesNet:
                 startIdx = idx + 1
         
         if startIdx < idx:
-            self.parseAndAddNode(data[startIdx:idx+1])
+            self.parseAndAddNode(data[startIdx:])
         
     def calculate_probability(self, varNode, evidenceMap):
-        #varNode = self.nodeMap[var]
+
+        if varNode.isDecision():
+            return 1
         indexesToBeAdded = []
         if len(varNode.parents) == 0:
             indexesToBeAdded.append("+")
@@ -122,18 +140,77 @@ class BayesNet:
             node.printNode()
 
 def enumeration_ask(query, bayes_net):
-    
+        
     queryNum, queryDen = convertToJoint(query)
     evidence_vars = deepcopy(queryNum.evidenceMap)
     evidence_vars.update(queryNum.variableMap)
+    queryNumMap = deepcopy(evidence_vars)
+    if query.type == "EU":
+        evidence_vars[utilityNode[0].name] = "+"
     prob = enumerate_all(bayes_net, bayes_net.nodeList, evidence_vars)
     if queryDen != None:
         evidence_vars = deepcopy(queryDen.evidenceMap)
         evidence_vars.update(queryDen.variableMap)
-        probDen = enumerate_all(bayes_net, bayes_net.nodeList, evidence_vars)
+        for dNode in decisionNodes:
+            evidence_vars[dNode.name] = queryNumMap[dNode.name]
+            
+        if query.type == "EU":
+            evidence_vars[utilityNode[0].name] = "+"
+            probDen = enumerate_all(bayes_net, bayes_net.nodeList[:-1], evidence_vars)
+        else:
+            probDen = enumerate_all(bayes_net, bayes_net.nodeList, evidence_vars)
         prob = prob / probDen
-    prob = format(prob, '.2f')
-    print prob
+    if query.type == "P":
+        prob += 0.000000000000001
+        prob = format(prob, '.2f')
+    else:
+        prob = int(prob + 0.5)
+    output.write(str(prob) + "\n")
+
+def allCombinations(query):
+    combinations = [{}]
+    for variable in query.variables:
+        if variable in query.variableMap.keys():
+            continue
+        newList = []
+        for idx in range(len(combinations)):
+            newMapT = dict(combinations[idx])
+            newMapT[variable] = "+"
+            newList.append(newMapT)
+            newMapF = dict(combinations[idx]) 
+            newMapF[variable] = "-"
+            newList.append(newMapF)
+        combinations = newList
+    return combinations
+            
+
+def enumeration_ask_MEU(query, bayes_net):
+    
+    allCombs = allCombinations(query)
+    maxUtilValue = float('-inf')
+    for comb in allCombs:
+        queryNum, queryDen = convertToJoint(query)
+        evidence_vars = deepcopy(queryNum.evidenceMap)
+        evidence_vars.update(comb)
+        evidence_vars[utilityNode[0].name] = "+"
+        prob = enumerate_all(bayes_net, bayes_net.nodeList, evidence_vars)
+        if queryDen != None:
+            evidence_vars = deepcopy(queryDen.evidenceMap)
+            evidence_vars.update(queryDen.variableMap)
+            evidence_vars.update(comb)
+            evidence_vars[utilityNode[0].name] = "+"
+            probDen = enumerate_all(bayes_net, bayes_net.nodeList[:-1], evidence_vars)
+            prob = prob / probDen
+        if prob > maxUtilValue:
+            maxUtilValue = prob
+            decisionQueryMap = comb
+    stro = ""
+    for variable in query.variables:
+        if variable not in query.variableMap.keys():
+            stro += decisionQueryMap[variable] + " "
+    stro += str(int(maxUtilValue + 0.5))
+    output.write(stro + "\n")
+
 
 def convertToJoint(query):
     if len(query.evidences) == 0:
@@ -165,11 +242,6 @@ def enumerate_all(bayes_net, variablesRem, evidenceMap):
             return true_probability_var + false_probability_var
 
 
-def normalize(tProb, fProb):
-    #To do : IMPLEMENT
-    
-    return
-
 def parseQuery(query, inp):
     vars = inp.split('|')
     queryVars = vars[0].strip(' \t\n\r').split(',')
@@ -177,6 +249,21 @@ def parseQuery(query, inp):
         eqSplit = queryVar.strip(' \t\n\r').split('=')
         query.variables.append(eqSplit[0].strip(' \t\n\r'))
         query.variableMap[eqSplit[0].strip(' \t\n\r')] = eqSplit[1].strip(' \t\n\r')
+    if len(vars) > 1:
+        evidenceVars = vars[1].strip(' \t\n\r').split(',')
+        for evVar in evidenceVars:
+            eqSplit = evVar.strip(' \t\n\r').split('=')
+            query.evidences.append(eqSplit[0].strip(' \t\n\r'))
+            query.evidenceMap[eqSplit[0].strip(' \t\n\r')] = eqSplit[1].strip(' \t\n\r')
+
+def parseMEUQuery(query, inp):
+    vars = inp.split('|')
+    queryVars = vars[0].strip(' \t\n\r').split(',')
+    for queryVar in queryVars:
+        eqSplit = queryVar.strip(' \t\n\r').split('=')
+        query.variables.append(eqSplit[0].strip(' \t\n\r'))
+        if len(eqSplit) > 1:
+            query.variableMap[eqSplit[0].strip(' \t\n\r')] = eqSplit[1].strip(' \t\n\r')
     if len(vars) > 1:
         evidenceVars = vars[1].strip(' \t\n\r').split(',')
         for evVar in evidenceVars:
@@ -197,7 +284,7 @@ def parseQueries(input):
             parseQuery(query, str.strip(' \t\r\n')[3:-1])
         elif str.startswith("MEU"):
             query.type = "MEU"
-            parseQuery(query, str.strip(' \t\r\n')[4:-1])
+            parseMEUQuery(query, str.strip(' \t\r\n')[4:-1])
         
         queries.append(query)
     return queries
@@ -218,7 +305,13 @@ if __name__ == "__main__":
     queries = parseQueries(dataLines[0:index])
     bayes_net.parseInputGraph(dataLines[index+1:])
     for query in queries:
-        enumeration_ask(query, bayes_net)
+        if query.type == "MEU":
+            enumeration_ask_MEU(query, bayes_net)
+        else:
+            enumeration_ask(query, bayes_net)
     
+    output.seek(0,2)
+    nsize = output.tell()
+    output.truncate(nsize-2)
         
     
